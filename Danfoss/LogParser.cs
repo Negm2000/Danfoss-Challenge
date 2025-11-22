4fe5f10fb3753b15a554bd3019264665e9a9a0b6
Karim Negm <karimyasser8@gmail.com>
2025-11-22 16:18:07 +0100

    Optimized LogParser.cs by making each regex insantiated only once.

--- Danfoss/LogParser.cs
+++ Danfoss/LogParser.cs
@@ -3,34 +3,35 @@ namespace Danfoss;
 using System.Text.RegularExpressions;
 
 public static class LogParser
-{
+{   // Captures one of the following
+    // Pattern 1: entry that begins with TEXT-TEXT-TEXT
+    // Pattern 2: entry that begins with [TEXT] TEXT-TEXT-TEXT
+    private static readonly Regex TimestampRegex = new Regex(@"^(\S+-\S+-\S+) |^\S+ (\S+-\S+-\S+)");
+    // Captures anything in the form of [TEXT]
+    private static readonly Regex ErrorRegex = new Regex(@"\[(\S+)\]");
+    // Captures all text after the first dash, until a new line begins.
+    private static readonly Regex MessageRegex = new Regex(@" - (.*)");
     public static String? GetTimestamp(String entry)
     {
         // Pattern 1: entry that begins with TEXT-TEXT-TEXT
+        var firstPattern = TimestampRegex.Match(entry).Groups[1];
         // Pattern 2: entry that begins with [TEXT] TEXT-TEXT-TEXT
-        var timestampRegex = new Regex(@"^(\S+-\S+-\S+) |^\S+ (\S+-\S+-\S+)");
-        var firstPattern = timestampRegex.Match(entry).Groups[1];
-        var secondPattern = timestampRegex.Match(entry).Groups[2];
+        var secondPattern = TimestampRegex.Match(entry).Groups[2];
         if (!firstPattern.Success && !secondPattern.Success) return null;
         return firstPattern.Success ? firstPattern.Value : secondPattern.Value;
     }
 
     public static String? GetLogLevel(String entry)
     {
-        // Captures anything in the form of [TEXT]
-        Regex errorRegex = new Regex(@"\[(\S+)\]");
         // Groups[0] is [TEXT] - Groups[1] is TEXT without the square brackets
-        var match = errorRegex.Match(entry).Groups[1];
+        var match = ErrorRegex.Match(entry).Groups[1];
         return match.Success ? match.Value : null; // No error code
     }
 
     public static String? GetMessage(String entry)
     {
-        // Captures all text after the first dash, until a new line begins.
-        // Multi-line logic handled in main code
-        Regex messageRegex = new Regex(@" - (.*)");
         // Groups[1] is the message without the ' - '
-        var match = messageRegex.Match(entry).Groups[1];
+        var match = MessageRegex.Match(entry).Groups[1];
         return match.Success ? match.Value : null;
     }
 }
